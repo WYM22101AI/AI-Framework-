06899f30d8624d48ef57cd0aea737ce30d2ad41c
Yaming Wang <yamingwang@gmail.com>
2026-05-20 23:36:53 -0400

    Add Alpaca API test script

---
+++ test_alpaca.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+from alpaca.data.historical import StockHistoricalDataClient
+from alpaca.data.requests import StockLatestQuoteRequest
+
+load_dotenv()
+
+API_KEY = os.getenv("APCA_API_KEY")
+API_SECRET = os.getenv("APCA_API_SECRET")
+
+client = StockHistoricalDataClient(API_KEY, API_SECRET)
+
+request = StockLatestQuoteRequest(symbol_or_symbols="AAPL")
+
+quote = client.get_stock_latest_quote(request)
+
+print("AAPL Latest Quote:")
+print(quote)
